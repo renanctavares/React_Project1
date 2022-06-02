f0e2c9058d74adba7d30b6850ac050662167e1d7
Renan Costa Tavares <r.costa.tavares@gmail.com>
2022-06-01 22:22:33 -0300

    Add link pages and localStorage saving

--- gamagit/src/Pages/Home/index.js
+++ gamagit/src/Pages/Home/index.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
 import * as S from './styled';
+import { useNavigate } from "react-router-dom";
 
 function App(props) {
+  const navigate = useNavigate();
   const [ usuario, setUsuario ] = useState('');
+  
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(Response => console.log(Response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(Response => {
+      const repositories = Response.data;
+      const repositoriesName = [];
+      repositories.map((repositories) => {
+        repositoriesName.push(repositories.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      navigate('/repositories');
+    });
   }
   return (
     <S.Container>
